a78ceb817c38c3958046a2643a64147659afc668
yemrekeskin <yemrekeskin@gmail.com>
2020-02-12 23:05:20 +0300

    randome note problem

--- Src/LeftRotation/Program.cs
+++ Src/LeftRotation/Program.cs
@@ -4,6 +4,7 @@ namespace LeftRotation
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -18,18 +19,54 @@ namespace LeftRotation
             Console.ReadLine();
         }
 
-        // Complete the rotLeft function below.
-        static int[] rotLeft(int[] a, int d)
+        static int[] rotLeft(int[] arr, int d)
         {
-            int remainingRotateCount = d % a.Length;
+            var result = new int[arr.Length]; // 5
+            
+            // 5 - 1 = 4
+            var effective = arr.Length - (d % arr.Length);
 
-            var result = new int[a.Length];
-            for (int i = remainingRotateCount; i < a.Length ; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                var item = a[i];
-                result[i - remainingRotateCount] = item;
+                result[(i + effective) % arr.Length] = arr[i];
             }
+
             return result;
         }
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+
+        //    //An array of integers .
+        //    //An integer, the number of rotations.
+
+        //    int[] a = { 1, 2, 3, 4, 5 };
+        //    int d = 3;
+        //    int[] result = rotLeft(a, d);
+
+        //    Console.ReadLine();
+        //}
+
+        //static int[] rotLeft(int[] arr, int d)
+        //{
+        //    var temp = arr;
+        //    for (int i = 0; i < d; i++)
+        //    {
+        //        temp = swap(temp);
+        //    }
+        //    return temp;
+        //}
+
+        //static int[] swap(int[] arr)
+        //{
+        //    var first = arr[0];
+        //    for (int i = 1; i < arr.Length; i++)
+        //    {
+        //        arr[i - 1] = arr[i];
+        //    }
+        //    arr[arr.Length - 1] = first;
+        //    return arr;
+        //}
     }
 }
---
+++ Src/RansomNote/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RansomNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] m = { "give", "me", "one", "grand", "today", "night" };
+            string[] n = { "give", "one", "grand", "today" };
+
+            checkMagazine(m, n);
+
+            string[] m1 = { "two", "times", "three", "is", "not", "four" };
+            string[] n1 = { "two", "times", "two", "is", "four" };
+
+            checkMagazine(m1, n1);
+
+            string[] m2 = { "ive", "got", "a", "lovely", "bunch", "of", "coconuts" };
+            string[] n2 = { "ive", "got", "some", "coconuts" };
+
+            checkMagazine(m2, n2);
+
+
+            Console.ReadLine();
+        }
+
+        static void checkMagazine(string[] magazine, string[] note)
+        {
+            var dic = new SortedDictionary<string, int>();
+
+            foreach (var group in magazine.GroupBy(x => x))
+            {
+                dic.Add(group.Key, group.Count());
+            }
+
+            foreach (var word in note.GroupBy(x => x))
+            {
+                var groupCount = dic.TryGetValue(word.Key, out int count);
+                if (!groupCount || word.Count() > count)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Yes");
+        }
+    }
+}
